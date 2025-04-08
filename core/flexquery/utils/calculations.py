5e12137cedc6c9f51b932b498843b58a3b09fa3d
miroslawsteblik <154693995+miroslawsteblik@users.noreply.github.com>
2025-04-08 20:48:02 +0100

    Update calculations.py

--- core/flexquery/utils/calculations.py
+++ core/flexquery/utils/calculations.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 
-class SmartpathCommsCalculations:
+class CommsCalculations:
     def __init__(self, df):
         self.df = df
 
@@ -15,3 +15,21 @@ class SmartpathCommsCalculations:
         """
         policy_numbers = self.df[self.df['HasEmail'] == 0]['PolicyNumber']
         return policy_numbers
+
+    def save_to_excel(self, output_file):
+        """Save query results DataFrame to an Excel file."""
+
+        with pd.ExcelWriter(output_file, engine='xlsxwriter') as writer:
+            # All data
+            all_data = self.all_tab()
+            all_data.to_excel(writer, sheet_name='All_Records', index=False)
+
+            # Format the Excel file
+            for sheet_name in writer.sheets:
+                worksheet = writer.sheets[sheet_name]
+                # Add filtering capability
+                worksheet.autofilter(0, 0, 0, len(all_data.columns) - 1)
+                # Freeze top row
+                worksheet.freeze_panes(1, 0)
+                
+        return output_file
